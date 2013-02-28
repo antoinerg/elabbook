75434090427517cc0f93fcfffa95d6c399b74a59
antoinerg <roygobeil.antoine@gmail.com>
2013-02-28 16:15:55 -0500

    Close to something final for scanita data

--- public/js/humanmsg.js
+++ public/js/humanmsg.js
@@ -20,63 +20,30 @@ var humanMsg = {
 		// Opacity of the message
 		humanMsg.msgOpacity = 0.8;
 
-		if (msgOpacity != undefined) 
-			humanMsg.msgOpacity = msgOpacity;
-
 		// Inject the message structure
-		jQuery(appendTo).append('<div id="'+humanMsg.msgID+'" class="humanMsg"><div class="round"></div><p></p><div class="round"></div></div> <div id="'+humanMsg.logID+'"><p>'+logName+'</p><ul></ul></div>')
+		jQuery(appendTo).append('<div id="'+humanMsg.logID+'"><p>'+logName+'</p><ul></ul></div>');
 		
 		jQuery('#'+humanMsg.logID+' p').click(
 			function() { jQuery(this).siblings('ul').slideToggle() }
-		)
-	},
+		);
 
+		// Initialy show
+		jQuery('#'+humanMsg.logID+' p').siblings('ul').slideToggle();	
+	},
 	displayMsg: function(msg) {
 		if (msg == '')
 			return;
 
-		clearTimeout(humanMsg.t2);
-
-		// Inject message
-		jQuery('#'+humanMsg.msgID+' p').html(msg)
+		jQuery('#'+humanMsg.logID)
+			.show().children('ul').prepend('<li>'+msg+'</li>')	// Prepend message to log
+			.children('li:first').slideDown(200)			// Slide it down
 	
-		// Show message
-		jQuery('#'+humanMsg.msgID+'').show().animate({ opacity: humanMsg.msgOpacity}, 200, function() {
-			jQuery('#'+humanMsg.logID)
-				.show().children('ul').prepend('<li>'+msg+'</li>')	// Prepend message to log
-				.children('li:first').slideDown(200)				// Slide it down
-		
-			if ( jQuery('#'+humanMsg.logID+' ul').css('display') == 'none') {
-				jQuery('#'+humanMsg.logID+' p').animate({ bottom: 0 }, 200, 'linear', function() {
-					jQuery(this).animate({ bottom: 0 }, 300, function() { jQuery(this).css({ bottom: 0 }) })
-				})
-			}
+		if ( jQuery('#'+humanMsg.logID+' ul').css('display') == 'none') {
+			jQuery('#'+humanMsg.logID+' p').animate({ bottom: 0 }, 200, 'linear', function() {
+				jQuery(this).animate({ bottom: 0 }, 300, function() { jQuery(this).css({ bottom: 0 }) })
+			})
+		}
 			
-		})
-
-		// Watch for mouse & keyboard in .5s
-		humanMsg.t1 = setTimeout("humanMsg.bindEvents()", 700)
-		// Remove message after 5s
-		humanMsg.t2 = setTimeout("humanMsg.removeMsg()", 5000)
-	},
-
-	bindEvents: function() {
-	// Remove message if mouse is moved or key is pressed
-		jQuery(window)
-			.mousemove(humanMsg.removeMsg)
-			.click(humanMsg.removeMsg)
-			.keypress(humanMsg.removeMsg)
-	},
-
-	removeMsg: function() {
-		// Unbind mouse & keyboard
-		jQuery(window)
-			.unbind('mousemove', humanMsg.removeMsg)
-			.unbind('click', humanMsg.removeMsg)
-			.unbind('keypress', humanMsg.removeMsg)
-
-
-			jQuery('#'+humanMsg.msgID).animate({ opacity: 0 }, 500, function() { jQuery(this).hide() })
 	}
 };
 
