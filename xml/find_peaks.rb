9c99d4ee50745224c2eaa7f27f67e05b7f184b1f
antoinerg <roygobeil.antoine@gmail.com>
2013-05-07 16:31:14 -0400

    Some scripts to summarize - incomplete

--- xml/find_peaks.rb
+++ xml/find_peaks.rb
@@ -1,16 +1,35 @@
 require_relative '../app.rb'
 config_file File.join( [settings.root, 'config', 'config.yml'] )
-require 'nokogiri'
-
 require_relative '../lib/find.rb'
+
+log = Logger.new(STDOUT)
+log.level = Logger::ERROR
+
+final = Nokogiri::XML('<summary/>')
+summary = final.at_xpath('/summary')
 
 find(File.join(settings.dir,'data/lt-afm/scanita')) do |f|
   if f.match(/.*xml/)
+    log.info("Analyzing #{f}")
     doc = Nokogiri::XML(File.read(f))
-    fits=doc.xpath("/SPM/Package/UserChannel[Name = 'Dissipation peaks']/UserData/Fitting//peak")
 
-    if !fits.empty?
-      puts fits
-    end
+    fit=doc.xpath("/SPM/Package/UserChannel[Name = 'Dissipation peaks' and Direction = 'Forward']/UserData/Fitting")
+    peaks = fit.xpath('//peak')
+    if !fit.empty?
+	    begin
+	      id=doc.at_xpath("/SPM/UserData/metadata/NP/@id").value
+	      puts id
+	    rescue
+	      log.error("#{f} not identified")
+	      next
+	    end
+	node=Nokogiri::XML::Node.new "NP", final
+	node["id"] = id
+	summary.add_child(node)
+	node.add_child(fit)
+    end 
   end
 end
+fd = File.open('summary.xml','w')
+final.write_xml_to(fd)
+fd.close
