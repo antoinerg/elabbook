ce03a3d890b8710dd123ab31c82ad64813985e31
antoine <antoine@ubuntu-desktop.(none)>
2013-05-08 11:33:16 -0400

    By default redirect to index.erb.html if presents

--- app.rb
+++ app.rb
@@ -111,6 +111,9 @@ get '*' do
   path = path(params[:splat] || '')
   if File.directory?(path)
     @folders = list_file(path)
+    if @folders.detect {|f| f.match(/.*index.erb.html$/)}
+      redirect File.join('/',params[:splat],'index.erb.html')
+    end
     @path = params[:splat][0] 
     erb :index, :layout => :html5
   else
